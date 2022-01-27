9799509a67b7af406a26453b6c7db15bdf3ba09a
cliffmax85 <cliffmax85@gmail.com>
2022-01-26 16:58:04 -0800

    almost working cars not rendering yet

--- src/Vehicle.js
+++ src/Vehicle.js
@@ -1,12 +1,15 @@
 
-export default function Vehicle() {
+export default function Vehicle(props) {
   return <div className="vehicle">
     {/* the Vehcile component has a prop called vehicle. Check to see what string is in this vehicle prop */}
 
     {/* if the vehicle props is a car, render a '🚗' emoji */}
-    
+    {props.type === 'car' && '🚗'}
     {/* if the vehicle props is a truck, render a '🚚' emoji */}
+    {props.type === 'truck' && '🚚'}
     {/* if the vehicle props is a bus, render a '🚌' emoji */}
+    {props.type === 'bus' && '🚌'}
     {/* if the vehicle props is a motorcycle, render a '🏍️' emoji */}
+    
   </div>;
 }
